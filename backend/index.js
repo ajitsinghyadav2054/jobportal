fd1f1481a42f308a9cabdf991467577c2eb7f972
Ajit Kumar <ajitsinghyadav2054@gmail.com>
2025-03-31 16:22:22 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({});
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    callback(null, true);
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 5001;
+
+//api's
+app.use("/api/user", userRoute);
+app.use("/api/company", companyRoute);
+app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Catch-all route to serve the frontend
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/utils/data.js
@@ -0,0 +1,6 @@
+const server="http://localhost:5011"
+
+export const USER_API_ENDPOINT = `${server}/api/user`;
+export const JOB_API_ENDPOINT = `${server}/api/job`;
+export const APPLICATION_API_ENDPOINT = `${server}/api/application`;
+export const COMPANY_API_ENDPOINT = `${server}/api/company`;
